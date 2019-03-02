89d82026cb7508691e6f5df32b772ab5c32e9f25
Yudel Martinez <yam3nv@virginia.edu>
2019-03-01 20:55:58 -0500

    Made data gathering tool, no bugs so far.
    Requires user to make own config file in format:
    ADB Tools Path,ABSOLUTE_PATH
    Save Path,ABSOLUTE_PATH
    Device ID,DEVICE_ID
    >> Device ID is found by using command adb devices.

---
+++ GetData.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+import datetime
+
+# --- Initialization ---
+variables = open('config').read().strip().replace('"','').split('\n')
+for i in range(len(variables)):
+    variables[i] = variables[i].strip().split('=')
+
+platform_tools = variables[0][1]
+data_path = variables[1][1]
+save_path = variables[2][1]
+device_1_id = variables[3][1]
+# ----------------------
+
+
+device_1_connected = False
+
+
+def storeData():
+    """Gets data from device with device ID and stores the data in the determined data save path in config file"""
+    this_save_path = save_path+'/'+device_1_id+'/'+str(datetime.datetime.now()).replace(' ','_').replace(':','-').replace('.','-')+'/'
+    os.makedirs(this_save_path)
+    process = subprocess.Popen([platform_tools,'-s',device_1_id,'pull',data_path,this_save_path])
+    while process.poll() == None:
+        pass
+    print('Data saved to: '+ this_save_path)
+    return 
+
+def pullData():
+    """Checks if device is connected and if it is and has not been downloaded and takes appropriate action"""
+    global device_1_connected
+    with subprocess.Popen([platform_tools,'devices'], stdout=subprocess.PIPE) as process:
+        output = str(process.stdout.read())
+        if device_1_id in output:
+            print('Device is Present...')
+            if device_1_connected:
+                print('Device was already Polled')
+                pass
+            else:
+                print('Gathering Device Data...')
+                storeData()
+                device_1_connected = True
+        else:
+            device_1_connected = False
+        process.kill()
