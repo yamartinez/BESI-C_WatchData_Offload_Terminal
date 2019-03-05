1bc9d04e70f165f28088d0b654acc8935976292d
Yudel Martinez <yam3nv@virginia.edu>
2019-03-05 10:47:24 -0500

    Implemented backup and cleanup
    data is now moved to timestamped folder in backup directory after being pulled

--- GetData.py
+++ GetData.py
@@ -11,38 +11,63 @@ for i in range(len(variables)):
 platform_tools = variables[0][1]
 data_path = variables[1][1]
 save_path = variables[2][1]
-device_1_id = variables[3][1]
+backup_path = variables[3][1]
+device_1_id = variables[4][1]
 # ----------------------
 
 
 device_1_connected = False
 data_saved = False
 
+def init():
 
-def storeData():
+    return
+
+def storeData(device_id):
     """Gets data from device with device ID and stores the data in the determined data save path in config file"""
-    this_save_path = save_path+'/'+device_1_id+'/'+str(datetime.datetime.now()).replace(' ','_').replace(':','-').replace('.','-')+'/'
+    timestamp = str(datetime.datetime.now()).replace(' ','_').replace(':','-').replace('.','-')
+    this_save_path = save_path+'/'+device_id+'/'+timestamp+'/'
     os.makedirs(this_save_path)
-    process = subprocess.Popen([platform_tools,'-s',device_1_id,'pull',data_path,this_save_path])
+    process = subprocess.Popen([platform_tools,'-s',device_id,'pull',data_path,this_save_path], stdout=subprocess.PIPE)
+    
     while process.poll() == None:
         pass
-    return this_save_path
+    output = str(process.stdout.read())
+    if ('0 files pulled' in output):
+        print("Data Acquisition Failed...")
+        return False
+    elif ('Not a directory' in output):
+        print("Failed")
+        return False
+    else:
+        dataBackup(timestamp)
+        return this_save_path
+
+def dataBackup(timestamp):
+    global backup_path
+    backup_path = backup_path +timestamp+'/'
+    process = subprocess.Popen([platform_tools,'-s',device_1_id,'shell','mkdir',backup_path], stdout=subprocess.PIPE)
+    process = subprocess.Popen([platform_tools,'-s',device_1_id,'shell','mv',data_path+'*', backup_path], stdout=subprocess.PIPE)
+    while process.poll() == None:
+        pass
+    return
 
 
 def pullData():
     """Checks if device is connected and if it is and has not been downloaded and takes appropriate action"""
     global device_1_connected,data_saved
 
+    
     with subprocess.Popen([platform_tools,'devices'], stdout=subprocess.PIPE) as process:
         output = str(process.stdout.read())
        
         if device_1_id in output:
             if (not device_1_connected) or (data_saved == False):
                 print('Device Connected\nGathering Device Data...')
-                datapath = storeData()
+                datapath = storeData(device_1_id)
                 device_1_connected = True
 
-                if os.path.isdir(datapath+data_path.split('/')[-2]):
+                if datapath:
                     data_saved = True
                     print('Data saved to: ' + datapath)
                 else:
